ac7a17d716f0816c9199385f816045f3582ec40b
Hiren <hiren7575@gmail.com>
2025-03-11 22:44:15 -0400

    Added maven surefire plugin and executed tests using maven

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -8,7 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+import com.constants.Browser;
 import com.ui.pages.HomePage;
 import com.utility.BrowserUtility;
 import com.utility.LambdaTestUtility;
@@ -16,18 +19,22 @@ import com.utility.LambdaTestUtility;
 public class TestBase {
 
 	protected HomePage homePage;
-	private boolean isLambdaTest = true;
-	private boolean isHeadless = true;
+	private boolean isLambdaTest;
 	
+	@Parameters({"browser", "isLambdaTest", "isHeadless"})
 	@BeforeMethod(description = "Load the homepage of the application")
-	public void setup(ITestResult result) {
+	public void setup(
+			@Optional("chrome") String browser, 
+			@Optional("true") boolean isLambdaTest, 
+			@Optional("false") boolean isHeadless, ITestResult result) {
+		this.isLambdaTest = isLambdaTest;
 		WebDriver lambdadriver;
 		if(isLambdaTest) {
-			lambdadriver = LambdaTestUtility.initializeLambdaTestSession("chrome", result.getMethod().getMethodName());
+			lambdadriver = LambdaTestUtility.initializeLambdaTestSession(browser, result.getMethod().getMethodName());
 			homePage = new HomePage(lambdadriver);
 		}
 		else {
-		 homePage = new HomePage(EDGE,isHeadless);
+		 homePage = new HomePage(Browser.valueOf(browser.toUpperCase()),isHeadless);
 		}
 	}
 	
